1643f28664c0f01a967f273fa762e716d792411f
AtifKhan@928100000 <atif.Khan92810000@gmail.com>
2025-11-02 02:21:00 +0500

    first

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const fs = require('fs');
+
+app.use(express.json());
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.get('/', (req, res) => {
+
+    fs.readdir(`./files`, (err, files) => {
+      res.render('index',{files:files}); 
+    });
+        
+});
+
+app.post('/create', (req, res) => {
+     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details , function(err) {
+         res.redirect("/")
+     })
+      
+});
+
+
+app.get('/files/:filename', (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`, 'utf8', (err, data) => {
+       res.render('show', {filename: req.params.filename , data: data});
+    });
+
+      
+});
+
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+}); 
